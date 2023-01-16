1cabe26360c35e6df3c0ae59fe815a4c556be4c8
AdonyasG <adonyas2000@gmail.com>
2023-01-16 15:01:49 -0500

    initial commit

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3i
+"""
+Requests Data From API
+"""
+
+import json
+import requests
+from sys import argv
+
+
+def information():
+    """return info from api"""
+    r = requests.get("https://jsonplaceholder.typicode.com/users")
+    todo = requests.get("https://jsonplaceholder.typicode.com/todos")
+    data = r.text
+    datas = todo.text
+    parse = json.loads(data)
+    parses = json.loads(datas)
+    TOTAL_NUM_OF_TASKS = 0
+    TASK_TITLE = []
+    NUMBER_OF_DONE_TASKS = 1
+    for i in parse:
+        if i.get('id') == int(argv[1]):
+            EMPLOYEE_NAME = (i.get('name'))
+            break
+    for i in parses:
+        if i.get('userId') == int(argv[1]):
+            TOTAL_NUM_OF_TASKS += 1
+            if i.get('completed') is True:
+                NUMBER_OF_DONE_TASKS += 1
+                TASK_TITLE.append(i.get('title'))
+    print("Employee {} is done with tasks({}/{}):".format(EMPLOYEE_NAME,
+                                                          NUMBER_OF_DONE_TASKS,
+                                                          TOTAL_NUM_OF_TASKS))
+    for i in TASK_TITLE:
+        print("\t", i)
+
+
+if __name__ == "__main__":
+    information()
