4303a72676a87b28a5981d2241dd5f82b655a965
AdonyasG <adonyas2000@gmail.com>
2023-01-31 03:58:52 -0500

    pagination added

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Requests Data From API
+"""
+
+import json
+import requests
+
+
+def top_ten(subreddit):
+    """find number of subscribers"""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+
+    headers = requests.utils.default_headers()
+    headers.update(
+                   {
+                       'User-Agent': 'User Agent 1.0',
+                    }
+                  )
+    response = requests.get(url, headers=headers, allow_redirects=False).json()
+    post = response.get('data', {}).get('children', [])
+    if post:
+        for i in post:
+            print(i.get('data').get('title'))
+    else:
+        pri
