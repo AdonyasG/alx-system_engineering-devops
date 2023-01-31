be1be10836d867d462fb94b33b93b974cff4aa42
AdonyasG <adonyas2000@gmail.com>
2023-01-31 14:05:05 -0500

    test

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,34 +1,36 @@
 #!/usr/bin/python3
 """
-Requests Data From API
+Query Reddit API recursively for all hot articles of a given subreddit
 """
-
 import requests
 
 
-def recurse(subreddit, hot_list=[], after="aft"):
-    """find number of subscribers"""
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-
+def recurse(subreddit, hot_list=[], after="tmp"):
+    """
+        return all hot articles for a given subreddit
+        return None if invalid subreddit given
+    """
+    # get user agent
+    # https://stackoverflow.com/questions/10606133/ -->
+    # sending-user-agent-using-requests-library-in-python
     headers = requests.utils.default_headers()
-    headers.update(
-                   {
-                       'User-Agent': 'User Agent 1.0',
-                    }
-                  )
-    if after != "aft":
+    headers.update({'User-Agent': 'My User Agent 1.0'})
+
+    # update url each recursive call with param "after"
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    if after != "tmp":
         url = url + "?after={}".format(after)
+    r = requests.get(url, headers=headers, allow_redirects=False)
 
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    post = response.json().get('data', {}).get('children', [])
-
-    if not post:
+    # append top titles to hot_list
+    results = r.json().get('data', {}).get('children', [])
+    if not results:
         return hot_list
-    for i in post:
-        hot_list.append(i.get('data').get('title'))
+    for e in results:
+        hot_list.append(e.get('data').get('title'))
 
-    after = response.json().get('data').get('after')
-
+    # get next param "after" else nothing else to recurse
+    after = r.json().get('data').get('after')
     if not after:
         return hot_list
     return (recurse(subreddit, hot_list, after))
