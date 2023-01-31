ff5463c72b80b3177951c5ca67cf0bc2574a8b9d
AdonyasG <adonyas2000@gmail.com>
2023-01-31 14:10:57 -0500

    test

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,36 +1,34 @@
 #!/usr/bin/python3
 """
-Query Reddit API recursively for all hot articles of a given subreddit
+Requests Data From API
 """
+
 import requests
 
 
-def recurse(subreddit, hot_list=[], after="tmp"):
-    """
-        return all hot articles for a given subreddit
-        return None if invalid subreddit given
-    """
-    # get user agent
-    # https://stackoverflow.com/questions/10606133/ -->
-    # sending-user-agent-using-requests-library-in-python
-    headers = requests.utils.default_headers()
-    headers.update({'User-Agent': 'My User Agent 1.0'})
-
-    # update url each recursive call with param "after"
+def recurse(subreddit, hot_list=[], after="aft"):
+    """find number of subscribers"""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    if after != "tmp":
+
+    headers = requests.utils.default_headers()
+    headers.update(
+                   {
+                       'User-Agent': 'User Agent 1.0',
+                    }
+                  )
+    if after != "aft":
         url = url + "?after={}".format(after)
-    r = requests.get(url, headers=headers, allow_redirects=False)
 
-    # append top titles to hot_list
-    results = r.json().get('data', {}).get('children', [])
-    if not results:
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    post = response.json().get('data', {}).get('children', [])
+
+    if not post:
         return hot_list
-    for e in results:
-        hot_list.append(e.get('data').get('title'))
+    for i in post:
+        hot_list.append(i.get('data').get('title'))
 
-    # get next param "after" else nothing else to recurse
-    after = r.json().get('data').get('after')
+    after = response.json().get('data').get('after')
+
     if not after:
         return hot_list
     return (recurse(subreddit, hot_list, after))
