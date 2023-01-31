bebe5b5a786440769121bd967f76cff8d205ea72
AdonyasG <adonyas2000@gmail.com>
2023-01-31 02:49:27 -0500

    pycodestyle fix

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Requests Data From API
+"""
+
+import json
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """find number of subscribers"""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+
+    headers = requests.utils.default_headers()
+    headers.update(
+                   {
+                       'User-Agent': 'User Agent 1.0',
+                    }
+                  )
+    response = requests.get(url, headers=headers).json()
+    subs = response.get('data', {}).get('subscribers')
+
+    if subs:
+        return subs
+    else:
+        return 0
