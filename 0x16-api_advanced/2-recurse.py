866955e7826ea894a51b66cf868c28fb8f8bd2ec
AdonyasG <adonyas2000@gmail.com>
2023-02-01 13:34:57 -0500

    if clause fix

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,34 +1,24 @@
 #!/usr/bin/python3
-"""
-Requests Data From API
-"""
 
 import requests
+import sys
 
 
-def recurse(subreddit, hot_list=[], after="aft"):
-    """find number of subscribers"""
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-
-    headers = requests.utils.default_headers()
-    headers.update(
-                   {
-                       'User-Agent': 'User Agent 1.0',
-                    }
-                  )
-    if after != "aft":
-        url = url + "?after={}".format(after)
-
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    post = response.json().get('data', {}).get('children', [])
-
-    if not post:
+def recurse(subreddit, hot_list=[], after=None):
+    """returns count of hotlist"""
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {'User-Agent': 'User agent- 1.0'}
+    params = {'limit': 100, 'after': after}
+    r = requests.get(url, headers=headers,
+                     params=params, allow_redirects=False)
+    if r.status_code == 200:
+        data = r.json().get('data')
+        after = data.get('after')
+        children = data.get('children')
+        for child in children:
+            hot_list.append(child.get('data').get('title'))
+        if after is not None:
+            recurse(subreddit, hot_list, after)
         return hot_list
-    for i in post:
-        hot_list.append(i.get('data').get('title'))
-
-    after = response.json().get('data').get('after')
-
-    if not after:
-        return hot_list
-    return (recurse(subreddit, hot_list, after))
+    else:
+        return None
